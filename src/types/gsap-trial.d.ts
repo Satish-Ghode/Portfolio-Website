4fbd3ff50c2bc44293688236a9ea2b58ba32a71d
Satish Ghode <satishghode9075@gmail.com>
2026-04-05 19:30:05 +0530

    Migrate from deprecated gsap-trial to regular gsap package

--- src/types/gsap-trial.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'gsap-trial/SplitText' {
-  import { SplitText as GSAPSplitText } from 'gsap/SplitText';
-  export const SplitText: typeof GSAPSplitText;
-  export { SplitText as default };
-}
