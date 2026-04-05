559ebb61de8e1b1b8afa82da35ab0d8eabe32df2
Satish Ghode <satishghode9075@gmail.com>
2026-04-05 19:22:03 +0530

    Fix TypeScript errors for gsap-trial/SplitText

---
+++ src/types/gsap-trial.d.ts
@@ -0,0 +1,5 @@
+declare module 'gsap-trial/SplitText' {
+  import { SplitText as GSAPSplitText } from 'gsap/SplitText';
+  export const SplitText: typeof GSAPSplitText;
+  export { SplitText as default };
+}
